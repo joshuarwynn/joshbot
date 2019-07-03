605df2f2b3f77e8fb97e23b693a029ccbfa0cb3f
joshuarwynn <joshua.wynn@gmail.com>
2019-07-02 21:51:47 -0600

    feat(issue1-base-structure): Adding GET /health endpoint.

--- app/server.js
+++ app/server.js
@@ -5,16 +5,29 @@ const morgan = require('morgan')
 const path = require('path')
 const log = require('./utils/log')
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, '../logs/access.log'), { flags: 'a' })
+const accessLogStream = fs.createWriteStream(
+  path.join(
+    __dirname,
+    (process.env.NODE_ENV !== 'test') ? '../logs/access.log' : '../logs/access.test.log'
+  ),
+  { flags: 'a' }
+)
+
 const app = express()
 const port = config.get('app.port')
 const router = express.Router()
 const urlencoded = express.urlencoded({ extended: true })
 
+// Express configuration
+app.set('x-powered-by', false)
+
 // Initialize middleware
 app.use(morgan('combined', { stream: accessLogStream }))
 app.use(router)
 router.use(urlencoded)
+
+// Initialize routes
+router.use(require('./health/health'))
 
 app.start = function start() {
   app.listen(port, (error) => {
--- app/utils/log.js
+++ app/utils/log.js
@@ -2,12 +2,14 @@ const winston = require('winston')
 
 const level = process.env.LOG_LEVEL || 'info'
 
-// This creates a new instance of Winston to combined.log
+// This creates a new instance of Winston to combined.log or combined.test.log
 const log = winston.createLogger({
   level,
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({ filename: './logs/combined.log' })
+    new winston.transports.File({
+      filename: (process.env.NODE_ENV !== 'test') ? './logs/combined.log' : './logs/combined.test.log'
+    })
   ]
 })
 

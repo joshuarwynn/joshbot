03a424e4af0b7c008c3d2e9f5d499d1d4e9eea2f
joshuarwynn <joshua.wynn@gmail.com>
2019-07-12 20:16:50 -0600

    chore(issue7-sqs-endpoints): Adding a JWT verification middleware.

---
+++ app/utils/jwt.js
@@ -0,0 +1,45 @@
+import config from 'config'
+import jwt from 'jsonwebtoken'
+import isUndefined from 'lodash/isUndefined.js'
+import log from './log.js'
+
+const jwtSecretKey = config.get('jwt.secretKey')
+
+/**
+ * Verifies that a JWT is valid and its payload contents
+ * @param  {Request}   req
+ * @param  {Response}   res
+ * @param  {Function} next
+ * @return {Object}
+ */
+function verifyJwt(req, res, next) {
+  if (!isUndefined(req.headers.authorization) && req.headers.authorization.split(' ')[0] === 'Bearer') {
+    // Authorization header is present and includes the Bearer schema
+    const token = req.headers.authorization.split(' ')[1]
+
+    try {
+      const payload = jwt.verify(token, jwtSecretKey)
+      const time = Math.floor(new Date().getTime() / 1000)
+
+      if (payload.admin === true && payload.exp >= time) {
+        // The JWT payload contains admin access is true and hasn't expired,
+        // so pass control to the next middleware function
+        next()
+      } else {
+        // Deny access to requestor
+        log.info(`While requesting ${req.path}, JWT payload does not contain admin access and/or has expired.`, { details: payload })
+        res.status(401).send()
+      }
+    } catch (error) {
+      // The JWT could not be verified, so deny access to requestor
+      log.info(`While requesting ${req.path}, JWT could not be verified.`)
+      res.status(401).send()
+    }
+  } else {
+    // Authorization header is missing or Bearer schema is not included
+    log.info(`While requesting ${req.path}, the authorization header or Bearer schema is missing.`)
+    res.status(401).send()
+  }
+}
+
+export default { verifyJwt }
